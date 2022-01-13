5997c814266621cf5b94c332b0f1c4a77455abc6
Code Palace <codepalace@fastmail.se>
2022-01-13 13:47:17 +0100

    First Commit

--- SwipeActions/ContentView.swift
+++ SwipeActions/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nameList = ["Mario", "Luigi", "Toad", "Peach", "Bowser"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                ForEach(nameList, id: \.self) { name in
+                    Text(name)
+                        .padding()
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                withAnimation(.easeInOut(duration: 2)) {
+                                    removeName(name: name)
+                                }
+                            } label: {
+                                Label("Delete", systemImage: "trash.fill")
+                            }
+                        }
+                        .swipeActions {
+                            Button {
+                                // Edit something
+                            } label: {
+                                Label("Edit", systemImage: "square.and.pencil")
+                            }
+                            .tint(.blue)
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                // Add Logic
+                            } label: {
+                                Label("Add", systemImage: "mic.fill")
+                            }
+                            .tint(.green)
+                        }
+                        
+            
+                }
+            }
+        }
+    }
+    func removeName(name: String) {
+        if let index = nameList.firstIndex(of: name) {
+            nameList.remove(at: index)
+        }
     }
 }
 
