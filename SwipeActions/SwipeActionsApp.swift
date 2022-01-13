33cbbdad203454a5eb2cb87dd0d58b17e7516313
Code Palace <codepalace@fastmail.se>
2022-01-13 13:05:51 +0100

    Initial Commit

---
+++ SwipeActions/SwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionsApp.swift
+//  SwipeActions
+//
+//  Created by Federico on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
